758e7f6c9e5c02bef92b98de1bbf160f8fc0a1d7
CDGA77 <danielgaviriaaguiar@gmail.com>
2024-05-15 13:30:26 -0500

    creacion y coneccion a la base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '190.147.64.47',
+      port: 5431,
+      username: 'riwi_user',
+      password: 'abcd1234',
+      database: 'books_2',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
